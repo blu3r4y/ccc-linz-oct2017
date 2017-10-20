922aeb7f9a3eea2d5b8fd6298a63718467d0786e
Mario Kahlhofer <mario.kahlhofer@gmail.com>
2017-10-20 10:33:28 +0200

    Create project skeletons

---
+++ ccc-csharp/ccc-csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ccc_csharp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
